e650ab83b40c31dabdcb8359480898fc71aec313
Cody Swann <cody@gunnertech.com>
2017-07-24 13:01:57 -0400

    deploy

--- deploy.js
+++ deploy.js
@@ -7,9 +7,10 @@ env.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 env.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 env.AWS_DEFAULT_REGION = process.env.AWS_DEFAULT_REGION;
 
-const spawn = require( 'child_process' ).spawnSync;
-const cmd = spawn('aws', [ 's3', 'sync', 'www/',
-    's3://' + process.env.S3_BUCKET_NAME + '/', '--delete'], {env: env});
+const spawn = require('child_process').spawnSync;
+const cmd = spawn('aws', ['s3', 'sync', 'www/',
+    's3://' + process.env.S3_BUCKET_NAME + '/', '--delete'
+], { env: env });
 
 console.log(cmd)
 
